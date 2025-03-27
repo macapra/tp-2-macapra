1af1d95cb7568e68f74c746da09bb0028180482e
macapra <mcapraprince@mail.austral.edu.ar>
2025-03-27 18:00:44 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,7 @@
 def ada():
-    first_name = "AdA"
+    first_name = "AdA "
     last_name = "LoVeLAce"
+    full_name= first_name + last_name
+    print(f"{full_name.lower()}")
+    print(f"{full_name.upper()}")
+    print(f"{full_name.title()}")
